a6489b80cb41c3896f2d20d6192ceab86839f0eb
Stefan Johner <sjohner@posteo.ch>
2016-08-23 00:19:02 +0200

    Add files via upload

---
+++ scu2016.php
@@ -0,0 +1,11 @@
+ <html>
+ <body>
+ <p align="center">
+ <img src="http://systemcenteruniverse.com/Content/Images/Layout/SCUEurope_blue.png" alt="SCU Logo" style="margin-top: 100px; margin-bottom: 30px;">
+ </p>
+ <h1 align="center" style="font-family: Karla, Arial, sans-serif; font-size: 48px; color: darkgrey"><?php echo gethostname(); ?></h1>
+ <h1 align="center" style="font-family: Karla, Arial, sans-serif; font-size: 34px; color: darkgrey"><?php echo date('Y-m-d H:i:s'); ?></h1>
+ </body>
+ </html>
+
+ 
