74802790f18d989a9d5c8d2f37b4b8324a54dc3d
Stefan Johner <sjohner@posteo.ch>
2016-08-22 23:46:42 +0200

    Update install_apache.sh

--- install_apache.sh
+++ install_apache.sh
@@ -2,7 +2,7 @@
 apt-get -y update
 
 # Install Apache2 and PHP
-apt-get -y install apache2 libapache2-mod-php5 php5
+apt-get -y install apache2 php7.0 libapache2-mod-php7.0
 
 # Create SCU2016 index.php
 cat << EOF > /var/www/html/index.php
@@ -11,7 +11,8 @@ cat << EOF > /var/www/html/index.php
  <p align="center">
  <img src="http://systemcenteruniverse.com/Content/Images/Layout/SCUEurope_blue.png" alt="SCU Logo" style="margin-top: 100px; margin-bottom: 30px;">
  </p>
- <h1 align="center" style="font-family: Karla, Arial, sans-serif; font-size: 54px;"><?php echo gethostname(); ?></h1>
+ <h1 align="center" style="font-family: Karla, Arial, sans-serif; font-size: 54px; color: darkgrey"><?php echo gethostname(); ?></h1>
+ <h1 align="center" style="font-family: Karla, Arial, sans-serif; font-size: 34px; color: darkgrey"><?php echo date('Y-m-d H:i:s'); ?></h1>
  </body>
  </html>
 EOF
